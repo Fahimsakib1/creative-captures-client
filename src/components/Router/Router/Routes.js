c92e847d1cd17b66e682674155f2d746f9e3e661
Fahim <60335100+Fahimsakib1@users.noreply.github.com>
2022-11-07 22:09:00 +0600

    set up the project and added the basic folder  structure

---
+++ src/components/Router/Router/Routes.js
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../../Home/Home/Home";
+import Layout from "../../Layout/Layout";
+
+const router = createBrowserRouter ([
+    {
+        path: '/',
+        element: <Layout></Layout>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
+
+export default router;
